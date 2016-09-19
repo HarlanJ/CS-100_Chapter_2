7736d6100417942a77482d35757df9af09ca299e
Tiduszk <clarkza@sunyit.edu>
2016-09-19 00:42:53 -0400

    Added algorithm

--- Book Exercies/C2-10.py
+++ Book Exercies/C2-10.py
@@ -1,12 +1,17 @@
-#Made by Zachary C. on 9/11/16 last edited on 9/16/16
-cookies = int(input('How many cookies would you like to make? '))		#Asks the user to input the number of cookies they want to make
+#Made by Zachary C. on 9/11/16 last edited on 9/19/16
+#1. Ask the user how many cookies to make
+#2. Save the number of cookies
+cookies = int(input('How many cookies would you like to make? '))
 
-#Calculates the ammount of each ingredient needed
+#3. Calculate the amount of each ingredient needed
+#4. Save the number of each ingredient needed
 percent_48 = cookies / 48
 sugar = percent_48 * 1.5
 butter = percent_48 * 1
 flour = percent_48 * 2.75
 
-#Displays the ammount of each ingredient needed
+#5. Displays the number of each ingredient needed
 print('To make' , cookies , 'cookies, you will need: ' , format(sugar, '.2f') , 'cups of sugar,' , \
 format(butter, '.2f') , 'cups of butter, and' , format(flour, '.2f') , 'cups of flour.')
+
+#6. End
--- Book Exercies/C2-9.py
+++ Book Exercies/C2-9.py
@@ -1,5 +1,13 @@
-#Made by Zachary C. on 9/11/16 last edited on 9/16/16
-C = float(input('Please enter the temperature in Celsius: '))		#Asks the user to input the temperature in Celsius
+#Made by Zachary C. on 9/11/16 last edited on 9/19/16
+#1. Ask the user to enter the temperature in Celsius
+#2. Save the user's input
+C = float(input('Please enter the temperature in Celsius: '))
+
+#3. Convert the input to Fahrenheit
+#4. Save the converted temperature
 F = (9 / 5) * C +32		#Converts the Celsius to Fahrenheit
 
-print(C , 'degrees Celsius is' , format(F, '.1f') , 'degrees Fahrenheit')		#Displays the converted Celsius and Fahrenheit
+#5. Display the converted temperature
+print(C , 'degrees Celsius is' , format(F, '.1f') , 'degrees Fahrenheit')
+
+#6. End
